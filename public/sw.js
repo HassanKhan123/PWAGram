b7c6195c8f6a0efa36cd47f9ce4a409a94bd9539
HassanKhan123 <hk2602809@gmail.com>
2020-09-08 06:17:15 +0500

    cache only strategy

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,28 @@
 const STATIC_CACHE = "static-v11";
 const DYNAMIC_CACHE = "dynamic";
+let STATIC_FILES = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/src/js/app.js",
+  "/src/js/feed.js",
+  "/src/js/promise.js",
+  "/src/js/fetch.js",
+  "/src/js/material.min.js",
+  "/src/css/app.css",
+  "/src/css/feed.css",
+  "/src/images/main-image.jpg",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+];
 
 self.addEventListener("install", (e) => {
   console.log("[SW] Installing service worker...", e);
   e.waitUntil(
     caches.open(STATIC_CACHE).then((cache) => {
       console.log("[SW] Precaching...");
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/offline.html",
-        "/src/js/app.js",
-        "/src/js/feed.js",
-        "/src/js/promise.js",
-        "/src/js/fetch.js",
-        "/src/js/material.min.js",
-        "/src/css/app.css",
-        "/src/css/feed.css",
-        "/src/images/main-image.jpg",
-        "https://fonts.googleapis.com/css?family=Roboto:400,700",
-        "https://fonts.googleapis.com/icon?family=Material+Icons",
-        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
-      ]);
+      cache.addAll(STATIC_FILES);
     })
   );
 });
@@ -53,7 +54,13 @@ self.addEventListener("fetch", (e) => {
         });
       })
     );
-  } else {
+  } 
+  // else if (
+  //   new RegExp("\\b" + STATIC_FILES.join("\\b|\\b") + "\\b").test(e.request.url)
+  // ) {
+  //   e.respondWith(caches.match(e.request));
+  // } 
+  else {
     e.respondWith(
       caches.match(e.request).then((res) => {
         if (res) {
@@ -68,8 +75,7 @@ self.addEventListener("fetch", (e) => {
             })
             .catch((err) => {
               return caches.open(STATIC_CACHE).then((cache) => {
-                if(e.request.url.indexOf('/help')){
-
+                if (e.request.url.indexOf("/help")) {
                   return cache.match("/offline.html");
                 }
               });
